a375f12037596ec6177dbf319603a4274841d4b6
arhamymr <m.arham@docotel.com>
2020-05-26 20:23:53 -0700

    improve animation

--- src/component/animations/index.jsx
+++ src/component/animations/index.jsx
@@ -1,16 +1,29 @@
 import anime from 'animejs/lib/anime.es.js';
 import gsap from "gsap"
+import MouseSpeed from "./speed"
 
 // cursor 
-
 export const animateExpandCursor = (expand, left) => {
 	gsap.to(".cursor",.1, { scale: expand })
 	gsap.to(".dot", .1, { left: left - 200 })
 }
 
 
-const mousemove = (top, left) => {
-	gsap.to(".cursor", 1, {top, left})
+const getAngle = (cursor, e) => {
+	let boxCenter=[
+		cursor.offsetLeft+cursor.clientWidth/2,
+		cursor.offsetTop+cursor.clientHeight/2];
+	let angle = Math.atan2(e.pageX - boxCenter[0], - (e.pageY - boxCenter[1]) )*(180/Math.PI);      
+	return angle
+}
+
+// speed = distance / time
+
+const mouse = new MouseSpeed()
+mouse.init()
+
+function mousemove(top, left, cursor,e){
+	gsap.fromTo(".cursor", 1, {rotate: `${getAngle(cursor, e)}deg`, width: `${25 - mouse.distance}px`},{width: "25px", top, left})
 	gsap.to(".dot", 1.1, {top, left})
 }
 
@@ -25,13 +38,14 @@ export const customCursor = () => {
 	
 	document.addEventListener("click", () => {
 		const tl = gsap.timeline()
-		tl.to(".dot", .2, {scale: 20, opacity: 0})
-		tl.fromTo(".dot",.2, {opacity: 0, scale: 20},{scale: 1, opacity: 1})
+		tl.to(".dot", .1, {scale: 20, opacity: 0})
+		tl.to(".dot",.1, {opacity: .5})
+		tl.to(".dot", .1, {scale: 1, opacity: 1})
 	})
 	document.addEventListener('mousemove', (e) => {
 		top = `${e.pageY - (cursor.clientHeight/2)}px`;
 		left = `${e.pageX - (cursor.clientWidth/2)}px`;
-		mousemove(top, left)
+		mousemove(top, left, cursor, e)
 	})
 
 	linkmerge.forEach( item => {
---
+++ src/component/animations/speed.jsx
@@ -0,0 +1,28 @@
+module.exports = class MouseSpeed {
+  
+  constructor() {
+    this.distance = 0
+    this.getDistance = this.getDistance.bind(this)
+    this.calcSpeed = this.calcSpeed.bind(this)
+  	this.convertToRange = this.convertToRange.bind(this)
+  }
+
+  convertToRange(val, max, min) { 
+  	return ((val - min) / (max - min)).toFixed(1); 
+  }
+
+  getDistance (p) {
+    const distance = Math.floor(Math.sqrt(Math.pow(p.x, 2) + Math.pow(p.y, 2)))
+    this.distance = Math.abs(this.convertToRange(distance, 25, 1))
+  }
+
+  calcSpeed(e) {
+    this.getDistance(
+      {x: e.movementX, y: e.movementY}
+    )
+  }
+
+  init() {
+    document.addEventListener("mousemove", this.calcSpeed);
+  }
+};
